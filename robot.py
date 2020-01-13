9185e5ae6b78d475453796b540909a49acdedb9f
eviatarbutin <eviatar2004@gmail.com>
2020-01-13 16:56:39 +0200

    adding some fixes to the code

--- robot.py
+++ robot.py
@@ -1,21 +1,24 @@
-from components.chassis import Chassis
 from components.pidcontroller import PIDControl
-from magicbot import MagicRobot
+from components.chassis import Chassis
 from wpilib.joystick import Joystick
+from magicbot import MagicRobot
 from ctre import WPI_TalonSRX
-import wpilib
 from navx import AHRS
+import wpilib
+
 
 class Robot(MagicRobot):
-    pidcontroller: PIDControl
+    pid_controller: PIDControl
     chassis: Chassis
     joystick: Joystick
 
     def createObjects(self):
         self.chassis_left_motor_master = WPI_TalonSRX(1)
         self.chassis_left_motor_slave = WPI_TalonSRX(2)
+
         self.chassis_right_motor_master = WPI_TalonSRX(3)
         self.chassis_right_motor_slave = WPI_TalonSRX(4)
+
         self.chassis_navx = AHRS.create_spi()
         self.joystick = Joystick(0)
 
